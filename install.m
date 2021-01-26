03c07741efd2934700b816b39347c7780e7fb444
Robert Damerius <damerius.mail@gmail.com>
2021-01-26 11:43:24 +0100

    Updated install/uninstall scripts.

--- install.m
+++ install.m
@@ -1,9 +1,18 @@
+fprintf('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n I N S T A L L   -  InterModuleProtocol\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n');
+
 % Add directories to MATLAB path
 addpath('lib');
 addpath(['lib' filesep 'driver']);
 
-% Save path settings
-savepath;
-
 % Compile sources
 run(['lib' filesep 'driver' filesep 'build.m']);
+
+% Save path
+str = input('Save path? [y]:  ','s');
+if(strcmp('y',str))
+    fprintf('Saving path\n');
+    savepath;
+else
+    fprintf('Path is not saved\n');
+end
+fprintf('Installation completed\n');
--- uninstall.m
+++ uninstall.m
@@ -1,6 +1,9 @@
+fprintf('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n U N I N S T A L L   -  InterModuleProtocol\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n');
+
 % Remove directories from MATLAB path
 rmpath('lib');
 rmpath(['lib' filesep 'driver']);
 
-% Save path settings
+% Save path
 savepath;
+fprintf('Uninstallation completed\n');
